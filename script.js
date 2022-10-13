be12c0d70a0bc437ecf73bda0856d9c453f8d93c
yuschenko-maria <yuschenko.maria@icloud.com>
2022-10-13 09:20:07 +0200

    add slider

---
+++ script.js
@@ -0,0 +1,29 @@
+let offset = 0;
+const carousel = document.querySelector('.carousel');
+let prevButton = document.querySelector('.prev-button'),
+nextButton = document.querySelector('.next-button');
+
+nextButton.addEventListener('click', function(){
+    offset += 140;
+    if (offset >= 420){
+        nextButton.setAttribute("disabled", "true");
+    }
+    else{
+        nextButton.disabled("false");
+        }
+    carousel.style.left = -offset + 'px';
+});
+prevButton.addEventListener('click', function(){
+    offset -= 140;
+    if (offset === 0){
+        prevButton.setAttribute("disabled", "true");
+    }
+    else{
+        prevButton.removeAttribute('disabled');
+    }
+    carousel.style.left = -offset + 'px';
+});
+
+
+
+ 
